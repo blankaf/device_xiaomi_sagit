bd0125aa0bc7d34f56c9cd394df10a2e4766644e
Luca Stefani <luca020400@lineageos.org>
2017-04-17 19:45:57 +0200

    sagit: Assert on modem build time-stamp
    * Trustzone isn't enough
    * Do >= check of the build time-stamp to avoid
      unnecessary commits on every new release
    
    Change-Id: I5a49c9571bba603af7873222e5383ec0494619e4

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,43 +19,19 @@ import common
 import re
 
 def FullOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.input_zip)
-  AddFileEncryptionAssertion(info)
+  AddModemAssertion(info, info.input_zip)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.target_zip)
-  AddFileEncryptionAssertion(info)
+  AddModemAssertion(info, info.target_zip)
   return
 
-def AddTrustZoneAssertion(info, input_zip):
+def AddModemAssertion(info, input_zip):
   android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
+  m = re.search(r'require\s+version-modem\s*=\s*(\S+)', android_info)
   if m:
     versions = m.group(1).split('|')
     if len(versions) and '*' not in versions:
-      cmd = 'assert(sagit.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
+      cmd = 'assert(sagit.verify_modem(' + ','.join(['"%s"' % modem for modem in versions]) + ') == "1");'
       info.script.AppendExtra(cmd)
   return
-
-def AddFileEncryptionAssertion(info):
-  info.script.AppendExtra('package_extract_file("install/bin/fbe_check.sh", "/tmp/fbe_check.sh");');
-  info.script.AppendExtra('set_metadata("/tmp/fbe_check.sh", "uid", 0, "gid", 0, "mode", 0755);');
-  info.script.AppendExtra('if !is_mounted("/data") then');
-  info.script.Mount("/data");
-  info.script.AppendExtra('endif;');
-  info.script.AppendExtra('if run_program("/tmp/fbe_check.sh") != 0 then');
-  info.script.AppendExtra('ui_print("===========================================");');
-  info.script.AppendExtra('ui_print("|              !!! ERROR !!!               ");');
-  info.script.AppendExtra('ui_print("|                                          ");');
-  info.script.AppendExtra('ui_print("| File-based Encryption is required.       ");');
-  info.script.AppendExtra('ui_print("|                                          ");');
-  info.script.AppendExtra('ui_print("| Backup your data (including internal     ");');
-  info.script.AppendExtra('ui_print("| storage) and format the data partition.  ");');
-  info.script.AppendExtra('ui_print("|                                          ");');
-  info.script.AppendExtra('ui_print("| Chinese: http://t.cn/R92kLm3             ");');
-  info.script.AppendExtra('ui_print("| English: https://t.co/7jDlsf2y6v         ");');
-  info.script.AppendExtra('ui_print("===========================================");');
-  info.script.AppendExtra('abort("FBE check failed.");');
-  info.script.AppendExtra('endif;');
-  info.script.Unmount("/data");
