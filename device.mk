499fbe743f88c1df800f84c3e50ad3668c9bc0a5
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    sagit: Move Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- device.mk
+++ device.mk
@@ -18,7 +18,9 @@
 $(call inherit-product-if-exists, vendor/xiaomi/sagit/sagit-vendor.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
