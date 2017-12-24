2fa8d85803fcb15db813962f70c92188ec10176a
jumoog <kilianvonpflugk@gmail.com>
2017-12-24 23:35:42 +0000

    sagit: re-enable Healthd
    Change-Id: Ie69182783d071ef0130d812a3cc1146c2d833abf

--- device.mk
+++ device.mk
@@ -211,6 +211,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service \
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
