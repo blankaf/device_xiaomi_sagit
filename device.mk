37464b1426646a323182f869c5c225810e1ea744
Bruno Martins <bgcngm@gmail.com>
2018-02-16 21:08:00 +0000

    sagit: Switch to vendor.display.config 1.1
     * No need to explicitly build the packages anymore,
       since they're now display HAL dependencies.
    
    Change-Id: Ied3be4c1e5fc7e01e0426268fb219c61a4be86e9

--- device.mk
+++ device.mk
@@ -169,8 +169,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
 
 # Doze
 PRODUCT_PACKAGES += \
