7bc767b9590898431cda7e7ae46b97e4b5670015
andr68rus <ashtmb@gmail.com>
2018-03-15 08:51:13 +0300

    sagit: prop: move hwui & dalvik to device.mk

--- device.mk
+++ device.mk
@@ -17,6 +17,29 @@
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/xiaomi/sagit/sagit-vendor.mk)
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heaptargetutilization=0.75
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.drop_shadow_cache_size=7 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.layer_cache_size=64 \
+    ro.hwui.path_cache_size=39 \
+    ro.hwui.r_buffer_cache_size=12 \
+    ro.hwui.text_large_cache_height=2048 \
+    ro.hwui.text_large_cache_width=3072 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.texture_cache_size=96
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -75,21 +75,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480 \
     vendor.display.enable_default_color_mode=1
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.drop_shadow_cache_size=7 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.layer_cache_size=64 \
-    ro.hwui.path_cache_size=39 \
-    ro.hwui.r_buffer_cache_size=12 \
-    ro.hwui.text_large_cache_height=2048 \
-    ro.hwui.text_large_cache_width=3072 \
-    ro.hwui.text_small_cache_height=2048 \
-    ro.hwui.text_small_cache_width=2048 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.texture_cache_size=96
-
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.cne.feature=1 \
@@ -142,15 +127,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     mmp.enable.3g2=true \
     persist.mm.enable.prefetch=true \
     vidc.enc.dcvs.extra-buff-count=2
-
-# Memory
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapmaxfree=8m \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heaptargetutilization=0.75
 
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
