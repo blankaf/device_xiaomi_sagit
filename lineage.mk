36f098e008e0c56e771df5067395613b9bc9916d
blankaf <realblankaf@gmail.com>
2018-03-21 19:27:07 +0100

    sagit: rework lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -3,18 +3,18 @@ $(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sagit
+# Device identifiers
 PRODUCT_DEVICE := sagit
+PRODUCT_NAME := lineage_sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_RELEASE_NAME := sagit
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="sagit" \
-    PRODUCT_NAME="sagit" \
-    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
+        PRODUCT_NAME=sagit \
+        PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys"
+BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
